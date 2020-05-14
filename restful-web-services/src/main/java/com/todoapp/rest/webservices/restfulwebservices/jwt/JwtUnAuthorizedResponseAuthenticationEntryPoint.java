8b4767e28b9b0fb4818d76bb45074b6ebd84da8c
Harsh Nagra <52031921+HarshNagra@users.noreply.github.com>
2020-05-15 02:16:01 +0530

    fixed the imports position - jwt backend

---
+++ restful-web-services/src/main/java/com/todoapp/rest/webservices/restfulwebservices/jwt/JwtUnAuthorizedResponseAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package com.todoapp.rest.webservices.restfulwebservices.jwt;
+
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+@Component
+public class JwtUnAuthorizedResponseAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+  private static final long serialVersionUID = -8970718410437077606L;
+
+  @Override
+  public void commence(HttpServletRequest request, HttpServletResponse response,
+                       AuthenticationException authException) throws IOException {
+    response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+        "You would need to provide the Jwt Token to Access This resource");
+  }
+}
